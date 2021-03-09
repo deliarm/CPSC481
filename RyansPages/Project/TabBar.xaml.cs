577b0c6b6bd113f16d6c24a8ce24d1c34cea7dc7
Ryan-Parent <46760042+Ryan-Parent@users.noreply.github.com>
2021-03-09 09:51:16 -0700

    Ryan's Pages

---
+++ RyansPages/Project/TabBar.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project
+{
+    /// <summary>
+    /// Interaction logic for TabBar.xaml
+    /// </summary>
+    public partial class TabBar : UserControl
+    {
+        public TabBar()
+        {
+            InitializeComponent();
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            add_popup.IsOpen = !add_popup.IsOpen;
+        }
+    }
+}
