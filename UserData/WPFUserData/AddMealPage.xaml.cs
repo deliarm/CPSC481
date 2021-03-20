d7bee3ec91b7b5be61c586769d2153faf0265535
steven <stevenloppe>
2021-03-20 11:24:46 -0600

    Switch from the old "Add Meal" pages to the new "Add Meal" page

--- UserData/WPFUserData/AddMealPage.xaml.cs
+++ UserData/WPFUserData/AddMealPage.xaml.cs
@@ -16,13 +16,67 @@ using System.Windows.Shapes;
 namespace WPFUserData
 {
     /// <summary>
-    /// Interaction logic for AddMealPage.xaml
+    /// Interaction logic for NewAddMealPage.xaml
     /// </summary>
     public partial class AddMealPage : Page
     {
         public AddMealPage()
         {
             InitializeComponent();
+
+            List<FoodItem> foodItems = new List<FoodItem>();
+            foodItems.Add(new FoodItem() { FoodName = "Waffles", Quantity = "x3", Calories=400 });
+            foodItems.Add(new FoodItem() { FoodName = "Apples", Quantity = "x1", Calories = 100 });
+            foodItems.Add(new FoodItem() { FoodName = "Eggs", Quantity = "x30", Calories = 2000 });
+
+            /*
+            foodItems.Add(new FoodItem() { FoodName = "Waffles", Quantity = "x3", Calories = 400 });
+            foodItems.Add(new FoodItem() { FoodName = "Apples", Quantity = "x1", Calories = 100 });
+            foodItems.Add(new FoodItem() { FoodName = "Eggs", Quantity = "x30", Calories = 2000 });
+
+            foodItems.Add(new FoodItem() { FoodName = "Waffles", Quantity = "x3", Calories = 400 });
+            foodItems.Add(new FoodItem() { FoodName = "Apples", Quantity = "x1", Calories = 100 });
+            foodItems.Add(new FoodItem() { FoodName = "Eggs", Quantity = "x30", Calories = 2000 });
+
+            foodItems.Add(new FoodItem() { FoodName = "Waffles", Quantity = "x3", Calories = 400 });
+            foodItems.Add(new FoodItem() { FoodName = "Apples", Quantity = "x1", Calories = 100 });
+            foodItems.Add(new FoodItem() { FoodName = "Eggs", Quantity = "x30", Calories = 2000 });
+            */
+
+            currentFoodList.ItemsSource = foodItems;
+
+            //TEMP: OBVIOUSLY THEY SHOULDN'T HAVE THE SAME CONTENTS
+            FoodOptionsList.ItemsSource = foodItems;
+        }
+
+        private void SearchCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            SearchPopup.Visibility = Visibility.Hidden;
+        }
+
+        private void FakeSearchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            SearchPopup.Visibility = Visibility.Visible;
+            RealSearchBox.Focus();
+        }
+
+        private void OverlayBackground_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            SearchPopup.Visibility = Visibility.Hidden;
+        }
+    }
+
+    public class FoodItem
+    {
+        public string FoodName { get; set; }
+        public string Quantity { get; set; }
+        public int Calories { get; set; }
+        public string CaloriesStr
+        {
+            get
+            {
+                return Calories + " cal";
+            }
         }
     }
 }
