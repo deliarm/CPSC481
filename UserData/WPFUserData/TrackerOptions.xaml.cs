1146de69e195aaf5e46d37cb1c3e1f6e940766a7
Ryan-Parent <ryan.parent@ucalgary.ca>
2021-03-19 21:20:41 -0600

    Updated Activity pages

--- UserData/WPFUserData/TrackerOptions.xaml.cs
+++ UserData/WPFUserData/TrackerOptions.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Windows.Threading;
 
 namespace WPFUserData
 {
@@ -20,9 +22,46 @@ namespace WPFUserData
     /// </summary>
     public partial class TrackerOptions : Page
     {
-        public TrackerOptions()
-        {
-            InitializeComponent();
-        }
-    }
+                DispatcherTimer dt = new DispatcherTimer();  
+        Stopwatch sw = new Stopwatch();  
+        string currentTime = string.Empty;  
+        public void MainWindow()   
+        {  
+            InitializeComponent();  
+            dt.Tick += new EventHandler(dt_Tick);  
+            dt.Interval = new TimeSpan(0, 0, 0, 0, 1);  
+        }  
+    
+        void dt_Tick(object sender, EventArgs e)   
+        {  
+            if (sw.IsRunning)   
+            {  
+                TimeSpan ts = sw.Elapsed;  
+                currentTime = String.Format("{0:00}:{1:00}:{2:00}",  
+                ts.Minutes, ts.Seconds, ts.Milliseconds / 10);  
+                ClockLabel.Content = currentTime;
+            }  
+        }  
+  
+        private void startbtn_Click(object sender, RoutedEventArgs e)  
+        {  
+            sw.Start();  
+            dt.Start();  
+        }  
+  
+        /*private void stopbtn_Click(object sender, RoutedEventArgs e)   
+        {  
+            if (sw.IsRunning)  
+            {  
+                sw.Stop();  
+            }  
+            elapsedtimeitem.Items.Add(currentTime);  
+        }  */
+  
+        private void resetbtn_Click(object sender, RoutedEventArgs e)   
+        {  
+            sw.Reset();  
+            ClockLabel.Content = "00:00:00";  
+        }  
+    } 
 }
