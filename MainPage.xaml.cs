a80985f084af857a21b138c991e2519d3ac9a16c
MichaelRediron <59679283+MichaelRediron@users.noreply.github.com>
2021-03-22 14:00:48 -0600

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Defaults;
+using LiveCharts.Configurations;
+
+
+namespace WPFUserData
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Percent = value => value.ToString() + "%";
+            Gauge.LabelFormatter = Percent;
+
+        }
+
+        public Func<Double, string> Percent { get; set; }
+    }
+}
