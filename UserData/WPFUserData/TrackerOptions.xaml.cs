788d9f9ce4ead0d014bc34a0319f8362f7dcaacc
Ryan-Parent <ryan.parent@ucalgary.ca>
2021-03-29 20:12:36 -0600

    Timer and distance tracker working, doesn't carry over to add activity page yet

--- UserData/WPFUserData/TrackerOptions.xaml.cs
+++ UserData/WPFUserData/TrackerOptions.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
 using System.Windows.Threading;
+using System.Timers;
 
 namespace WPFUserData
 {
@@ -22,46 +23,68 @@ namespace WPFUserData
     /// </summary>
     public partial class TrackerOptions : Page
     {
-                DispatcherTimer dt = new DispatcherTimer();  
-        Stopwatch sw = new Stopwatch();  
-        string currentTime = string.Empty;  
-        public void MainWindow()   
-        {  
-            InitializeComponent();  
-            dt.Tick += new EventHandler(dt_Tick);  
-            dt.Interval = new TimeSpan(0, 0, 0, 0, 1);  
-        }  
-    
-        void dt_Tick(object sender, EventArgs e)   
-        {  
-            if (sw.IsRunning)   
-            {  
-                TimeSpan ts = sw.Elapsed;  
-                currentTime = String.Format("{0:00}:{1:00}:{2:00}",  
-                ts.Minutes, ts.Seconds, ts.Milliseconds / 10);  
-                ClockLabel.Content = currentTime;
-            }  
-        }  
-  
-        private void startbtn_Click(object sender, RoutedEventArgs e)  
-        {  
-            sw.Start();  
-            dt.Start();  
-        }  
-  
-        /*private void stopbtn_Click(object sender, RoutedEventArgs e)   
-        {  
-            if (sw.IsRunning)  
-            {  
-                sw.Stop();  
-            }  
-            elapsedtimeitem.Items.Add(currentTime);  
-        }  */
-  
-        private void resetbtn_Click(object sender, RoutedEventArgs e)   
-        {  
-            sw.Reset();  
-            ClockLabel.Content = "00:00:00";  
-        }  
-    } 
+        DispatcherTimer dt = new DispatcherTimer();
+        Stopwatch sw = new Stopwatch();
+        string currentTime = string.Empty;
+        int distanceIncrement = 0;
+        int prevDistanceIncrement = 0;
+        double distanceTravelled = 0.0;
+
+        public TrackerOptions()
+        {
+            InitializeComponent();
+            dt.Tick += new EventHandler(dt_Tick);
+            dt.Interval = new TimeSpan(0,0,0,0,1);
+        }
+        
+        void dt_Tick(object sender, EventArgs e)
+        {
+            if (sw.IsRunning)
+            {
+                TimeSpan ts = sw.Elapsed;
+                currentTime = String.Format("{0:00}:{1:00}:{2:00}",
+                ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+                TimeDisplay.Text = currentTime;
+                distanceIncrement = ts.Minutes*4+ts.Seconds/15;
+                if (distanceIncrement > prevDistanceIncrement)
+                {
+                    prevDistanceIncrement = distanceIncrement;
+                    distanceTravelled = distanceTravelled + 0.1;
+                    DistanceDisplay.Text = distanceTravelled.ToString();
+                }
+            }
+        }
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sw.IsRunning)
+            {
+                sw.Stop();
+            }
+        }
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            sw.Start();
+            dt.Start();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            sw.Reset();
+            TimeDisplay.Text = "00:00:00";
+            DistanceDisplay.Text = "0.0";
+            distanceIncrement = 0;
+            prevDistanceIncrement = 0;
+            distanceTravelled = 0.0;
+        }
+        private void ManualAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("AddActivityPage.xaml", UriKind.Relative));
+        }
+
+        private void FinishButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("AddActivityPage.xaml", UriKind.Relative));
+        }
+    }
 }
